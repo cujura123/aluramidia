f4339ad344d04e847fb6eea14ec31faf8daa5c89
cujura123 <106816003+cujura123@users.noreply.github.com>
2023-10-05 11:01:27 -0300

    Update mains.js

--- mains.js
+++ mains.js
@@ -9,3 +9,14 @@ function tocaSom (seletorAudio) {
         //alert('Elemento não encontrado ou seletor inválido);
       }
     }
+
+
+    const litaDeTeclas = document.querySelectorAll ('.tecla');
+
+    //para
+    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+      const tecla = listaDeTeclas[contador];
+      const instrumento = tecla.classList[1];
+      const idAudio = `#som_${instrumento}`; //template string
+    }
