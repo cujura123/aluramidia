a45452d6c7970557f32fcc3f9d86933c7e7d587d
cujura123 <106816003+cujura123@users.noreply.github.com>
2023-10-19 11:06:47 -0300

    Update mains.js

--- mains.js
+++ mains.js
@@ -1,22 +1,38 @@
-function tocaSom (seletorAudio) {
-      const elemento = document.querySelector (seletorAudio);
+function toca Som(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
 
-      if (elemento && elemento. localName == 'audio') { 
-        elemento.pay()
+  if (elemento && elemento.localName === 'audio') {
+     elemento.play(); 
+  }
+  else{
+      //alert('Elemento não encontrado');
+      console.log('Elemento não encontrado ou seletor inválido');
+  }
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //template string
+
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (evento) {
+
+      if (evento.code === 'Space' || evento.code === 'Enter'){
+          tecla.classList.add('ativa');
       }
-      else{
-      //alert('Elemento não encontrado")
-        //alert('Elemento não encontrado ou seletor inválido);
-      }
-    }
 
+  }
 
-    const litaDeTeclas = document.querySelectorAll ('.tecla');
-
-    //para
-    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-      const tecla = listaDeTeclas[contador];
-      const instrumento = tecla.classList[1];
-      const idAudio = `#som_${instrumento}`; //template string
-    }
+  tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
+  }
+}
